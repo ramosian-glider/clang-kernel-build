5a884f92f033fbd0469c6f0e8ffcd162858ee623
Alexander Potapenko <glider@google.com>
2016-05-04 13:35:58 +0200

    Disable graphics in qemu

--- run_qemu.sh
+++ run_qemu.sh
@@ -10,5 +10,6 @@ sudo qemu-system-x86_64 -hda ${THISDIR}/wheezy.img -m 1024M -smp 4 \
 	-net user,hostfwd=tcp:127.0.0.1:${PORT}-:22 -net nic \
 	-kernel $KERNEL_SRC_DIR/arch/x86/boot/bzImage \
 	-append "console=ttyS0 root=/dev/sda earlyprintk=serial " \
+	-nographic \
 	-pidfile vm_pid  -enable-kvm
 
