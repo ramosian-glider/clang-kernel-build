a0f4ba1bfe42095dc76e59d8a0eb8898300f5c53
Alexander Potapenko <glider@google.com>
2016-11-22 18:36:50 +0100

    Updated clang_update_to_revision.sh

--- clang_update_to_revision.sh
+++ clang_update_to_revision.sh
@@ -8,12 +8,27 @@ function update_path_rev {
     cd $1
     if [ -z "${REVISION}" ]
     then
-      svn up
+      svn up || exit 1
     else
-      svn up -r ${REVISION}
+      svn up -r ${REVISION} || exit 1
     fi
   )
 }
+
+function get_llvm {
+  svn co http://llvm.org/svn/llvm-project/llvm/trunk .
+  cd tools
+  svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+  cd ../projects
+  svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+  svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+  svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+}
+
+if [ ! -d '.svn' ]; then
+  ls -la
+  get_llvm
+fi
 
 update_path_rev .
 update_path_rev tools/clang
@@ -21,6 +36,7 @@ update_path_rev projects/compiler-rt
 update_path_rev projects/libcxx
 update_path_rev projects/libcxxabi
 rm -rf llvm_cmake_build
+
 mkdir llvm_cmake_build && cd llvm_cmake_build
 #cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON `pwd`/../
 cmake  -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE='-O3 -DNDEBUG  -fno-omit-frame-pointer' `pwd`/../
