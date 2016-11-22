36783706a91262c636a7377d75dcc47a8e9edf0d
Alexander Potapenko <glider@google.com>
2016-11-22 18:37:48 +0100

    More KMSAN instrumentation
    TODO: move to a separate file?

--- clang_wrapper.py
+++ clang_wrapper.py
@@ -9,7 +9,8 @@ import time
 WORLD_PATH = os.path.dirname(os.path.abspath(__file__))
 
 COMPILER_PATH = {'gcc': 'gcc',
-  'clang': WORLD_PATH + '/third_party/llvm-build/Release+Asserts/bin/clang'
+  #'clang': WORLD_PATH + '/third_party/llvm-build/Release+Asserts/bin/clang'
+  'clang': '/home/glider/src/llvm/llvm_cmake_build/bin/clang',
 }
 
 FILTER = {'gcc': ['-Qunused-arguments', '-no-integrated-as', '-mno-global-merge',
@@ -54,11 +55,40 @@ def filter_args(argv, cname):
       new_argv.append(arg)
   return new_argv
 
+def want_msan_for_file(source):
+  starts = ['lib/r', 'lib/t', 'lib/g']
+  # Whacking the known reports.
+  starts += ['kernel/time/hrtimer.c']
+  starts += ['fs/proc/proc_sysctl.c', 'arch/x86/entry/vdso/vdso32-setup.c']
+  starts += ['fs/proc/']
+  starts += ['block/blk-ioc.c']
+  starts += ['kernel/irq/irqdesc.c']
+  starts += ['kernel/irq/proc.c']
+  starts += ['mm/vmalloc.c']
+  starts += ['kernel/exit.c', 'kernel/kmod.c', 'lib/kobject_uevent.c']
+  starts += ['drivers/base/']
+  ### hangs on boot
+  ### starts += ['kernel/sched']
+  ### starts += ['kernel/sched/fair.c']
+  ### # lib/idr.c initializes memory allocated in fs/proc
+  ### ['arch/x86/mm/ioremap.c', 'lib/idr.c']
+  ### oopsing at boot time
+  ###starts += ['arch/x86/kernel/apic/apic.c']
+  ###starts += ['lib/k']
+  if source.endswith('test_kmsan.c'):
+    return True
+  for i in starts:
+    if source.startswith(i):
+      return True
+  return False
+
+
 def msan_argv(flags, argv):
   source = flags[SOURCE]
   argv += ['-Wno-address-of-packed-member']
-  if source.endswith('test_kmsan.c'):
+  if want_msan_for_file(source):
     argv += ['-fsanitize=memory', '-mllvm', '-msan-kernel=1', '-mllvm', '-msan-keep-going=1']
+  #, '-fsanitize-memory-track-origins=2']
   return argv
 
 def compiler_argv(flags, argv):
