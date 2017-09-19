9bd6fe1ceb2e31a1e2fa2cae0f98217264768047
Alexander Potapenko <glider@google.com>
2017-09-19 23:13:34 +0200

    run_qemu.sh: minor improvements

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,14 +3,15 @@
 THISDIR=`dirname $0`
 KERNEL_SRC_DIR=${THISDIR}/linux-stable/
 PORT=10023
+SMP=4
 
 echo "run \`ssh -i ssh/id_rsa -p ${PORT} root@localhost\`"
 
-sudo qemu-system-x86_64 -hda ${THISDIR}/wheezy.img -m 1024M -smp 4 \
+qemu-system-x86_64 -hda ${THISDIR}/wheezy.img -m 1024M -smp ${SMP} \
 	-net user,hostfwd=tcp:127.0.0.1:${PORT}-:22 -net nic \
 	-kernel $KERNEL_SRC_DIR/arch/x86/boot/bzImage \
-	-append "console=ttyS0 root=/dev/sda earlyprintk=serial " \
+	-append "console=ttyS0 root=/dev/sda earlyprintk=serial" \
 	-nographic \
-	-s \
+	-s  \
 	-pidfile vm_pid  -enable-kvm
 
